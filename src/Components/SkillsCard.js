3be8656855abe64a9d0650b3707046da027bf53d
Sai1710 <91655800+Sai1710@users.noreply.github.com>
2023-03-06 19:48:14 +0530

    Update SkillsCard.js

--- src/Components/SkillsCard.js
+++ src/Components/SkillsCard.js
@@ -1,15 +1,10 @@
 import React from "react";
 
-function SkillsCard({ skill, skillPhoto }) {
+function SkillsCard({skillPhoto }) {
   return (
     <div className="flex justify-center p-5">
       <div className="block bg-white shadow-lg dark:bg-neutral-700 w-80 border-solid border-b border-2 rounded-3xl">
         <img className="rounded-t-lg h-60 w-80" src={skillPhoto} alt="" />
-        <div className="p-6">
-          <h5 className="mb-2 text-xl font-bold leading-tight text-red-700 text-center">
-            {skill}
-          </h5>
-        </div>
       </div>
     </div>
   );
