db1f4819065bb5717e62b5bb9cc72fd9f15b96dc
Sai1710 <91655800+Sai1710@users.noreply.github.com>
2023-03-06 19:50:00 +0530

    Update SkillsCard.js

--- src/Components/SkillsCard.js
+++ src/Components/SkillsCard.js
@@ -6,6 +6,11 @@ function SkillsCard({skillPhoto }) {
       <div className="block bg-white shadow-lg dark:bg-neutral-700 w-80 border-solid border-b border-2 rounded-3xl">
         <img className="rounded-t-lg h-60 w-80" src={skillPhoto} alt="" />
       </div>
+      <div className="p-6">
+          <h5 className="mb-2 text-xl font-bold leading-tight text-red-700 text-center">
+            {skill}
+          </h5>
+      </div>
     </div>
   );
 }
